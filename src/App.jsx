ba1978faf65869b5cb87332fa89ad53312be9c89
Nifemi <oluwaladen@gmail.com>
2025-01-02 23:24:37 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Features from "./components/Features.jsx";
+import Story from "./components/Story.jsx";
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-y-scroll">
+      <Navbar />
+      <Hero />
+      <About />
+      <Features />
+      <Story />
+    </main>
+  );
+};
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,61 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import AnimatedTitle from "./AnimatedTitle.jsx";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: "0",
+    });
+  });
+
+  return (
+    <div id="about" className="min-h-screen w-screen">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <h2 className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Zula
+        </h2>
+
+        <AnimatedTitle
+          title=" Expl<b>o</b>re the world's <br /> l<b>a</b>rgest shared platform"
+          containerClass="mt-5 !text-black text-cente"
+        />
+
+        <div className="about-subtext">
+          <p>
+            Embarking on grand MMORPG journey, The Game of Games is now your new
+            reality{" "}
+          </p>
+          <p>Every single player from every single game maybe found on Zula </p>
+        </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default About;
